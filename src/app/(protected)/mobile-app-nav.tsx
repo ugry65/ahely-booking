5f5775dce78121f4bc0fc05d310ecd8aef1c5c17
ugry65 <olahimre65@gmail.com>
2026-08-25 15:26:40 +0200

    feat: link pricing in mobile admin menu

--- src/app/(protected)/mobile-app-nav.tsx
+++ src/app/(protected)/mobile-app-nav.tsx
@@ -25,6 +25,7 @@ export function MobileAppNav({ displayName, isAdmin }: Props) {
           <Link href="/foglalasaim" onClick={closeMenu}>Foglalásaim</Link>
           <Link href="/adataim" onClick={closeMenu}>Adataim</Link>
           {isAdmin ? <Link href="/admin/felhasznalok" onClick={closeMenu}>Felhasználók</Link> : null}
+          {isAdmin ? <Link href="/admin/dijazas" onClick={closeMenu}>Díjazás</Link> : null}
           {isAdmin ? <Link href="/admin/helyisegek" onClick={closeMenu}>Helyiségek</Link> : null}
           {isAdmin ? <Link href="/admin/beallitasok" onClick={closeMenu}>Beállítások</Link> : null}
           {isAdmin ? <Link href="/admin/havi-orak" onClick={closeMenu}>Havi órák</Link> : null}
