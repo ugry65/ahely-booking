6a1433355f3bf864b8932d4da32916f33aad4a8a
ugry65 <olahimre65@gmail.com>
2026-09-02 19:45:46 +0200

    fix(pwa): use valid manifest icon purposes

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,13 +17,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/pwa-icon-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
       {
         src: "/pwa-icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
